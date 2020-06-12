d41d450ad77d6d2d90f592589e65f1487f7406a1
GenesizVitual <lastfandiansyah@gmail.com>
2020-06-12 10:31:38 +0800

    - Penambahan filter POS tidak boleh memilih produk yang sama, - penambahan max lenght pada stok produk

--- resources/views/AkuntansiDagang/Penjualan/partial/list_produk.blade.php
+++ resources/views/AkuntansiDagang/Penjualan/partial/list_produk.blade.php
@@ -1,11 +1,14 @@
-<div class="container_pembelian">
+<div class="container_pembelian_{{$data->id}}">
     <p>
         <input type="hidden" name="product_id[]" value="{{ $data->id }}">
         <input type="hidden" name="kwantitas_lama[]" value="{{ $data->stok }}">
         Product: <input class="form-control" name="nama_produk" value="{{ $data->nama_barang}}" readonly/>
     </p>
     <p>
-        Kwantitas: <input class="form-control" type="text" name="kwantitas[]" value="{{ $data->stok }}">
+        Kwantitas: <input class="form-control" type="number" max="{{$data->stok}}" name="kwantitas[]" value="{{ $data->stok }}">
+    </p>
+    <p class="pull-left">
+        <a href="#" class="btn btn-sm btn-danger" onclick="remove_order({{ $data->id }})"><i class="fa fa-eraser"></i> Batalkan pesanan</a>
     </p>
     <hr>
 </div>
